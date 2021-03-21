d04f2fc4be3c2f97f2a2453c5cd9741a4ee527b5
obaidmit <mdobaidarif98@gmail.com>
2021-03-22 01:56:24 +0530

    first git

---
+++ admin_area/includes/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$con=mysqli_connect("remotemysql.com","z4G95uZ8Z8","TlXuKh1wP2","z4G95uZ8Z8");
+
+if(mysqli_connect_errno()){
+	echo "Failed to connect to MySQL: ".mysqli_connect_error();
+}
+
+?>
